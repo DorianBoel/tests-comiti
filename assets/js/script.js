cff5c499f79c44e127416e6ab32228d123df7691
DorianBoel <dorian.boel@free.fr>
2022-12-04 12:46:09 +0100

    add js script w/ api fetch

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("contactForm");
+
+const firstNameInput = document.getElementById("firstNameInput");
+const lastNameInput = document.getElementById("lastNameInput");
+const phoneNumberInput = document.getElementById("phoneNumberInput");
+const dateOfBirthInput = document.getElementById("dateOfBirthInput");
+
+form.addEventListener("submit", (evt) => {
+    let reqData = {
+        "firstName": firstNameInput.value,
+        "lastName": lastNameInput.value,
+        "phoneNumber": phoneNumberInput.value
+    }
+    let dateOfBirth = dateOfBirthInput.value;
+    if (dateOfBirth) {
+        reqData.dateOfBirth = dateOfBirth;
+    }
+
+    evt.preventDefault();
+    fetch(`${API_URL}/contact`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(reqData)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+});
